97579fb456cada0b4e63f087b7328bba82c77645
deepakgla16 <deepaksharma972079@gmail.com>
2025-03-08 11:45:23 +0530

    Update Controller

--- src/main/java/com/example/AddressBook/repository/AddressBookRepository.java
+++ src/main/java/com/example/AddressBook/repository/AddressBookRepository.java
@@ -1,2 +1,7 @@
-package com.example.AddressBook.repository;public interface AddressBookRepository {
+package com.example.AddressBook.repository;
+
+import com.example.AddressBook.model.AddressBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressBookRepository extends JpaRepository<AddressBook, Long> {
 }
